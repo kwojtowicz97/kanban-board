c31233a462b99b90f607ffe528379245c7c9427f
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-11-22 18:44:12 +0100

    Initial commit

---
+++ src/components/LeftSidebar/LeftSidebar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './LeftSidebar.module.css'
+
+const LeftSidebar = () => {
+  return <div className={styles.leftSidebar}>LeftSidebar</div>
+}
+
+export default LeftSidebar
