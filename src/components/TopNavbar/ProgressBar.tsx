d4d64326f8c6c992a1ba0b9aa0f2d30b5939686c
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2022-12-06 18:57:32 +0100

    Added animation to ProgressBar

--- src/components/TopNavbar/ProgressBar.tsx
+++ src/components/TopNavbar/ProgressBar.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { paste } from '@testing-library/user-event/dist/paste'
+import React, { useContext, useState } from 'react'
+import { KanbanContext, TTask } from '../../providers/KanbanProvider'
 import styles from './ProgressBar.module.css'
 
 const ProgressBar = () => {
-  let progress = 0.1
+  const { projects, currentProject } = useContext(KanbanContext)
+  const [progress, setProgress] = useState(0)
+
+  const currentProj = projects.find(
+    (proj) => proj.projectName === currentProject
+  )
+
+  if (!currentProj) throw new Error()
+
+  const allTasks = currentProj.lists.reduce<TTask[]>(
+    (prev, curr) => prev.concat(curr.tasks),
+    []
+  )
+
+  const noOfLists = currentProj.lists.length
+
+  const finishedTasks = currentProj.lists[noOfLists - 1].tasks.length
+
+  console.log(allTasks.length, finishedTasks)
+
+  if (finishedTasks / allTasks.length - progress > 0.005) {
+    setTimeout(() => setProgress((state) => state + 0.002), 5)
+  } else if (finishedTasks / allTasks.length - progress < -0.005) {
+    setTimeout(() => setProgress((state) => state - 0.002), 5)
+  }
   const progressAngle = progress * 360
   return (
     <div data-progress={Math.round(progress * 100)} className={styles.progress}>
